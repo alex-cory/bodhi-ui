ab54635b7085ff5113b23945bb3765ea52f7897e
Lysisos <kyan_0626@yahoo.co.jp>
2018-09-18 15:52:25 -0700

    createevent test

---
+++ src/network/graphql/__mocks__/mutations.js
@@ -0,0 +1,106 @@
+/** This is a mock module of ../queries.js,
+ * so the test files link to this instead of connecting to backend
+ * This file simply skip the backend retrival and fake query results and return
+ * Current logic is straightforward but good enough to make tests
+ * */
+import mockData from './mockData';
+
+export function mockResetTopicList() {
+  mockData.resetTopics();
+}
+
+export function mockResetOracleList() {
+  mockData.resetOracles();
+}
+
+export function mockAddTopic(newTopic) {
+  mockData.addTopics(newTopic);
+}
+
+export function mockResetTransactionList() {
+  mockData.resetTransactions();
+}
+
+export function mockAddTransaction(newTransaction) {
+  mockData.addTransactions(newTransaction);
+}
+
+export function mockAddOracle(newOracle) {
+  mockData.addOracles(newOracle);
+}
+
+export function createTopic(
+  name,
+  results,
+  centralizedOracle,
+  bettingStartTime,
+  bettingEndTime,
+  resultSettingStartTime,
+  resultSettingEndTime,
+  escrowAmount,
+  senderAddress,
+) {
+  const newTransaction = {
+    createdTime: '',
+    status: 'PENDING',
+    token: 'BOT',
+    txid: '',
+    type: 'APPROVECREATEEVENT',
+    createdBlock: 0,
+    gasLimit: '100000000',
+    gasPrice: 0.01,
+    senderAddress,
+    version: 0,
+    name,
+    options: results,
+    resultSetterAddress: centralizedOracle,
+    bettingStartTime,
+    bettingEndTime,
+    resultSettingStartTime,
+    resultSettingEndTime,
+    amount: escrowAmount,
+  };
+
+  mockData.addTransactions(newTransaction);
+
+  // return temp transaction
+  return newTransaction;
+}
+
+export function createBetTx(version, topicAddress, oracleAddress, optionIdx, amount, senderAddress) {
+  const newTransaction = {
+    createdBlock: 0,
+    createdTime: 0,
+    gasLimit: '100000000',
+    gasPrice: 0.01,
+    senderAddress,
+    version,
+    topicAddress,
+    oracleAddress,
+    optionIdx,
+    token: 'QTUM',
+    amount,
+    type: 'BET',
+    status: 'PENDING',
+  };
+
+  mockData.addTransactions(newTransaction);
+
+  // return temp transaction
+  return newTransaction;
+}
+
+export function createSetResultTx(version, topicAddress, oracleAddress, optionIdx, amount, senderAddress) {
+}
+
+export function createVoteTx(version, topicAddress, oracleAddress, optionIdx, amount, senderAddress) {
+}
+
+export function createFinalizeResultTx(version, topicAddress, oracleAddress, senderAddress) {
+}
+
+export function createWithdrawTx(type, version, topicAddress, senderAddress) {
+}
+
+export function createTransferTx(senderAddress, receiverAddress, token, amount) {
+}
--- src/scenes/CreateEvent/store.test.js
+++ src/scenes/CreateEvent/store.test.js
@@ -1,6 +1,20 @@
+import { Routes } from 'constants';
+import axios from 'axios';
+import { isProduction } from '../../config/app';
 import CEStore from './store';
+import { decimalToSatoshi } from '../../helpers/utility';
 
+jest.mock('../../network/graphql/queries'); // block and manually mock our backend
+jest.mock('../../network/graphql/mutations');
 jest.mock('../../helpers/mixpanelUtil');
+
+jest.mock('axios');
+
+const TIME_DELAY_FROM_NOW_SEC = 15 * 60;
+let TIME_GAP_MIN_SEC = 30 * 60;
+if (!isProduction()) {
+  TIME_GAP_MIN_SEC = 2 * 60;
+}
 
 // setup mock
 const appMock = {
@@ -28,14 +42,56 @@ const appMock = {
 };
 
 // start test
-describe('CEStore', () => {
+describe('Create Event Store', () => {
   let store;
   beforeEach(() => {
     store = new CEStore(appMock);
   });
 
-  it('calculateBlock', () => {
-    store.setResultSetter('blacktea');
-    expect(store.resultSetter).toBe('blacktea');
+  describe('Open', () => {
+    beforeEach(async () => {
+      const respEscrow = {
+        0: decimalToSatoshi(1.5),
+      };
+      const respInsightTotals = {
+        n_blocks_mined: 598,
+        time_between_blocks: 999,
+        mined_currency_amount: 239200000000,
+        transaction_fees: 384590974074,
+        number_of_transactions: 1585,
+        outputs_volume: 11460269902794,
+        difficulty: 964275.6173266647,
+        stake: 0.0016828621038027618,
+      };
+      const respValidAddr = {
+        account: '',
+        address: '',
+        hdmasterkeyid: '',
+        iscompressed: true,
+        ismine: true,
+        isscript: false,
+        isvalid: true,
+        iswatchonly: false,
+        pubkey: '',
+        scriptPubKey: '',
+        timestamp: 1536777189,
+      };
+      axios.post.mockReturnValueOnce(respEscrow);
+      axios.get.mockReturnValueOnce(respInsightTotals);
+      axios.post.mockReturnValueOnce(respValidAddr);
+      await store.open();
+    });
+    it('test', () => {
+      // expect(store.escrowAmount).toBe(1.5);
+      expect(store.averageBlockTime).toBe(999);
+      expect(store.isOpen).toBe(true);
+    });
+  });
+
+  describe('Util Funcs', () => {
+    it('setResultSetter', () => {
+      store.setResultSetter('blacktea');
+      expect(store.resultSetter).toBe('blacktea');
+    });
   });
 });
